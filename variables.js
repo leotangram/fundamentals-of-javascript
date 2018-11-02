14d3014994f7ab8b0e7de246f7d516ec238787ac
Leonardo Omaña <leotangram@gmail.com>
2018-11-02 10:31:18 -0500

    variables

---
+++ variables.js
@@ -0,0 +1,12 @@
+/**
+ * Java Script es un lenguaje Debilmente Tipado
+ * Eso quiere decir que una variable puede ser string, o numero, 
+ * puede mutar sin ningun problema
+ */
+var nombre = 'Leo', apellido = 'Omaña'
+var edad = 28
+
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo ' + edad + ' años')
+
+var peso = 75
