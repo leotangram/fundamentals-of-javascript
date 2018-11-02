acf62121786d754e78ba21a018f4bab08f3a8eb4
Leonardo Omaña <leotangram@gmail.com>
2018-11-02 11:29:34 -0500

    Functions

---
+++ funciones.js
@@ -0,0 +1,10 @@
+var nombre = 'Leo', edad = 26
+
+function imprimirEdad(n, e) {
+  console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Vicky', 28)
+imprimirEdad('Eric', 24)
+imprimirEdad('Dario', 27)
