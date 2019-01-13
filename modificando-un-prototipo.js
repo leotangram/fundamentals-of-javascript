9bbf5141997023fcd979c6c24e7dbf755c464729
Leonardo Omaña <leonardoomana@Leonardos-MacBook-Pro.local>
2019-01-12 20:23:24 -0500

    The truth hidden about classes in Java SCript

--- modificando-un-prototipo.js
+++ modificando-un-prototipo.js
@@ -1,3 +1,10 @@
+function heredaDe(prototipoHijo, prototipoPadre) {
+  var fn = function () {}
+  fn.prototype = prototipoPadre.prototype
+  prototipoHijo.prototype = new fn
+  prototipoHijo.prototype.constructor = prototipoHijo
+}
+
 function Persona(nombre, apellido, altura) {
   this.nombre = nombre
   this.apellido = apellido
@@ -12,8 +19,20 @@ Persona.prototype.soyAlto = function () {
   return this.alura > 1.8
 }
 
-leo.soyAlto()
-hellen.soyAlto()
+function Desarrollador(nombre, apellido) {
+  this.nombre = nombre
+  this.apellido = apellido
+}
 
-var leo = new Persona('Leo', 'Omaña', 1.73)
-var hellen = new Persona('Hellen', 'Rozo', 1.65)
+heredaDe(Desarrollador, Persona)
+
+Desarrollador.prototype.saludar = function () {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador/a`)
+}
+
+
+// leo.soyAlto()
+// hellen.soyAlto()
+
+// var leo = new Persona('Leo', 'Omaña', 1.73)
+// var hellen = new Persona('Hellen', 'Rozo', 1.65)
